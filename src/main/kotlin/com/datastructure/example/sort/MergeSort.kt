1f887dc3d0a56ca590a2a969689aae7a19933696
quittypig <quittypig@bigpi.co>
2022-06-25 02:28:28 +0900

    ✨: merge sort

---
+++ src/main/kotlin/com/datastructure/example/sort/MergeSort.kt
@@ -0,0 +1,48 @@
+package com.datastructure.example.sort
+
+import org.w3c.dom.ranges.Range
+import java.util.stream.Stream
+import kotlin.math.ceil
+
+class MergeSort private constructor() {
+    companion object {
+        fun sort(numbers: MutableList<Int>) {
+            println("MergeSort Start! $numbers")
+
+            split(numbers)
+
+            println("MergeSort End!")
+        }
+
+        private fun split(numbers: MutableList<Int>): MutableList<Int> {
+            println("split: $numbers")
+
+            if (numbers.size == 1) {
+                return numbers
+            }
+
+            val middle = ceil((numbers.size.toDouble() / 2.toDouble())).toInt()
+            val left = split(numbers.subList(0, middle))
+            val right = split(numbers.subList(middle, numbers.size))
+
+            return merge(left, right)
+        }
+
+        private fun merge(numbers1: MutableList<Int>, numbers2: MutableList<Int>): MutableList<Int> {
+            val mergedNumbers: MutableList<Int> = mutableListOf()
+            val numbers = numbers1.plus(numbers2).toMutableList()
+
+            for (i in 1..numbers.size) {
+                numbers.minOf { it }.apply {
+                    mergedNumbers.add(this)
+
+                    numbers.remove(this)
+                }
+            }
+
+            println("merge: $mergedNumbers")
+
+            return mergedNumbers
+        }
+    }
+}
--- src/main/kotlin/com/datastructure/example/sort/SortMain.kt
+++ src/main/kotlin/com/datastructure/example/sort/SortMain.kt
@@ -6,4 +6,6 @@ fun main() {
     BubbleSort.sort(mutableListOf(5, 4, 3, 2, 1))
 
     InsertionSort.sort(mutableListOf(5, 4, 3, 2, 1))
+
+    MergeSort.sort(mutableListOf(5, 7, 6, 2, 1))
 }
